1ae92afb4c2f3e35b44e9ea41bd75b85992f0733
MGormley <matthewgormley005@gmail.com>
2025-05-01 21:23:30 +0100

    Developed Various Functions

--- MachineFleet/FleetDatabase.c
+++ MachineFleet/FleetDatabase.c
@@ -1,334 +1,324 @@
-/*
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <stdbool.h>
 
 typedef struct node
 {
-	int data;
+	char chassisNum[20];
+	char machineryMake[20];
+	char machineryModel[20];
+	int year;
+	double cost;
+	double currentValue;
+	double currentMileage;
+	double nextServiceMile;
+	char owner[30];
+	char email[30];
+	int phone[10];
+	int machineType; //1 = Tractor, 2 = Excavator, 3 = Roller, 4 = Crane, 5 = Mixer
+	int breakdownNum; //1 = never, 2 = <3, 3 = <5, 4 = >5
 	struct node* NEXT;
 	struct node* PREV;
-}nodeT;
+}machine;
+
+//Initialise psuedo-methods
 
 
-void addAtTheEnd(nodeT** end);
-void addAtTheStart(nodeT** top, nodeT** end);
-void addAtPosition(nodeT* top, int pos);
-//void addAtTheEndV2(nodeT* top, int newData);
-//void addAtTheStartV2(nodeT** top, int newData);
-
-void display(nodeT* top);
-void displayReverse(nodeT* end);
-int length(nodeT* top);
-int search(nodeT* top, int searchTerm);
-void deleteAtTheStart(nodeT** top, nodeT** end);
-void deleteAtTheEnd(nodeT** top, nodeT** end);
-
-int menu();
+//Main
 void main()
 {
-	nodeT* headPtr = NULL;
-	nodeT* tailPtr = NULL;
-	nodeT* newNode;
-	nodeT* prev = NULL;
-	int option;
-	int count;
-	int found;
-	int searchTerm;
-	FILE* ip;
-	int dataFromFile;
-	int position;
-	int numInputs;
+	//Initialise Machine Structs
+	machine* headPtr = NULL;
+	machine* tailPtr = NULL;
+	machine* newNode = NULL;
+	machine* prev = NULL;
 
+	//Variables
+	int option = -10, loginMode, machineType;
+	char machineOption[1], a[1] = "a", b[1] = "b", c[1] = "c", d[1] = "d", e[1] = "e", A[1] = "A", B[1] = "B", C[1] = "C", D[1] = "D", E[1] = "E";
 
+	//Load Files
 
-	option = menu();
+	//Prompt user login
+	loginMode = userLogin();
 
+	//Prompt initial user options
+	option = displayOptions();
+
+	//Sentinal While Loop
 	while (option != -1)
 	{
+		//Reset variables
+		machineOption;
+		machineType = 0;
+
+		//Option 1: Add machine
 		if (option == 1)
 		{
-			addAtTheStart(&headPtr, &tailPtr);
+
 		}
 
+		//Option 2: Display all machines to screen
 		else if (option == 2)
 		{
-			if (headPtr == NULL)
-				addAtTheStart(&headPtr, &tailPtr);
-			else
-				addAtTheEnd(&tailPtr);
+
 		}
 
+		//Option 3: Display machines details
 		else if (option == 3)
 		{
-			display(headPtr);
+
 		}
 
+		//Option 4: Update a machine's details
 		else if (option == 4)
 		{
-			displayReverse(tailPtr);
+
 		}
 
+		//Option 5: Delete machine
 		else if (option == 5)
 		{
-			printf("The length of the list is %d\n", length(headPtr));
+
 		}
 
+		//Option 6: Generate statistics (a - d) based on machinery type
 		else if (option == 6)
 		{
-			printf("Please enter the search integer\n");
-			scanf("%d", &searchTerm);
+			printf("\nPlease enter the machine you wish to see the statistics for:");
+			printf("(a) Tractor");
+			printf("(b) Excavator");
+			printf("(c) Roller");
+			printf("(d) Crane");
+			printf("(e) Mixer\n");
+			scanf("%s", machineOption);
 
-			found = search(headPtr, searchTerm);
+			//Tractor
+			if (strcmp(machineOption, a) == 0 || strcmp(machineOption, A) == 0)
+			{
+				machineType = 1;
+				displayStats(machineType); //Feed Fleet
+			}
 
-			if (found == 1)
-				printf("The number %d was found in the list\n", searchTerm);
+			//Excavator
+			else if (strcmp(machineOption, b) == 0 || strcmp(machineOption, B) == 0)
+			{
+				machineType = 2;
+				displayStats(machineType); //Feed Fleet
+			}
+
+			//Roller
+			else if (strcmp(machineOption, c) == 0 || strcmp(machineOption, C) == 0)
+			{
+				machineType = 3;
+				displayStats(machineType); //Feed Fleet
+			}
+
+			//Crane
+			else if (strcmp(machineOption, d) == 0 || strcmp(machineOption, D) == 0)
+			{
+				machineType = 4;
+				displayStats(machineType); //Feed Fleet
+			}
+
+			//Mixer
+			else if (strcmp(machineOption, e) == 0 || strcmp(machineOption, E) == 0)
+			{
+				machineType = 5;
+				displayStats(machineType); //Feed Fleet
+			}
+
+			//Invalid input
 			else
-				printf("The number was not found\n");
+			{
+				printf("\nInvalid input...\n");
+			}
 		}
 
+		//Option 7: Print all machine details into a report
 		else if (option == 7)
 		{
-			deleteAtTheStart(&headPtr, &tailPtr);
+
 		}
 
+		//Option 8: List all the machinery in order of current valuation
 		else if (option == 8)
 		{
-			deleteAtTheEnd(&headPtr, &tailPtr);
+
 		}
-		else if (option == 9)
+
+		//Option -1: End Program
+		else if (option == -1)
 		{
-			printf("Please enter the position you wish to add the number in\n");
-			scanf("%d", &position);
-
-			if (position == 1)
-				addAtTheStart(&headPtr, &tailPtr);
-			else if (position == length(headPtr) + 1)
-				addAtTheEnd(&tailPtr);
-			else if (position<1 || position> length(headPtr) + 1)
-				printf("This is not permitted\n");
-			else
-				addAtPosition(headPtr, position);
-
+			printf("\nEnding Program...\n");
 		}
 
-		option = menu();
+		//Invalid Input
+		else if (option != 1 && option != 2 && option != 3 && option != 4 && option != 5 && option != 6 && option != 7 && option != 8 && option != -1)
+		{
+			printf("\nInvalid input entered, please enter a valid input.\n");
+		}
+
+		//Ask for user option again at end of loop
+		option = displayOptions();
+
+	}//End of while Loop
+
+	//At end of loop run saveAll method
+
+}//End of main
+
+//userLogin -> prompts user for login
+int userLogin()
+{
+	int loginMode, choice;
+	bool attemptingLogin;
+	char username1[20], username2[20], adminName[20];
+	char password1[20], password2[20], adminPassword[20];
+	char usernameInput[20], passwordInput[20];
+
+	int loginTesting = 1;//Testing for login info 
+
+	//Open the logins file and feed them to the variables
+
+	//Prompt user to log in or quit
+	printf("Do you wish to log in (1 for yes, -1 for no): ");
+	scanf("%d", &choice);
+
+	//If the user is trying to log in
+	if (choice == 1)
+	{
+		while (choice == 1)
+		{
+			printf("\nAttempting login.\n");
+			//Prompt user for login details
+
+			//UnSuccessful login (-1)
+			if (loginTesting == 0)
+			{
+				loginMode = -1;
+				printf("\nUnsuccessful User Login\n");
+			}
+
+			//Successful login (1)
+			if (loginTesting == 1)
+			{
+				loginMode = 1;
+				printf("\nSuccessful User Login.\n");
+			}
+
+			//Admin Login (2)
+			if (loginTesting == 2)
+			{
+				loginMode = 2;
+				printf("\nSuccessful Admin Login.\n");
+			}
+
+			//Prompt user to log in or quit
+			printf("\nDo you wish to try again (1 for yes, -1 for no): ");
+			scanf("%d", &choice);
+		}
+
+		//Failed login attempt
+		if (choice == -1 && loginMode == -1)
+		{
+			printf("\nLogin failed, ending program...\n");
+		}
+	}//End of log in attempt
+
+	//If user does not want to attempt a log in
+	else
+	{
+		printf("\nNot attempting login, ending program...\n\n");
+		loginMode = -1;
 	}
 
-
+	return loginMode;
 }
 
-
-int menu()
+//displayOptions -> displays loop options to user
+int displayOptions()
 {
 	int option;
-
-	printf("Enter 1 to add a node at the beginning\n");
-	printf("Enter 2 to add a node at the end\n");
-	printf("Enter 3 to display all the nodes\n");
-	printf("Enter 4 to display all the nodes in reverse\n");
-	printf("Enter 5 to get the length of the list\n");
-	printf("Enter 6 to search the list\n");
-	printf("Enter 7 to delete at the start\n");
-	printf("Enter 8 to delete at the end\n");
-	printf("Enter 9 to add at a position\n");
-	printf("Enter -1 to exit\n");
+	printf("Please enter option: ");
 	scanf("%d", &option);
 
 	return option;
-
 }
 
-void addAtTheEnd(nodeT** end)
+//addMachine -> adds machine to position in LL based on Chassis Number (Unique)
+
+//displayAll -> display all machine's details on screen
+int displayAll()
 {
-	nodeT* newNode;
-	nodeT* temp;
-
-	temp = *end;
-
-	newNode = (nodeT*)malloc(sizeof(nodeT));
-	printf("Please enter the data for the new node\n");
-	scanf("%d", &newNode->data);
-
-	temp->NEXT = newNode;
-	newNode->PREV = temp;
-	newNode->NEXT = NULL;
-	*end = newNode;
 
 }
 
-void addAtTheStart(nodeT** top, nodeT** end)
+//displayStats -> display statistics of chosen machine types based on user input
+int displayStats(int machineType) //Feed fleet
 {
-	nodeT* newNode;
+	//Loop that tracks machine num and failures of the selected type
 
-	newNode = (nodeT*)malloc(sizeof(nodeT));
-	printf("Please enter the data for the new node\n");
-	scanf("%d", &newNode->data);
+	int machineCount = 0; //Update
+	int machineFailures = 0; //Update
 
-	newNode->PREV = NULL;
-	newNode->NEXT = *top;
+	int totalSuccesses = (machineCount - machineFailures);
+	float successesPercentage = (totalSuccesses / machineCount) * 100;
 
-	if (*top != NULL)
-		(*top)->PREV = newNode;
-
-	*top = newNode;
-
-	if (*end == NULL)
-		*end = newNode;
-}
-
-void addAtPosition(nodeT* top, int pos)
-{
-	nodeT* temp, * temp2;
-	nodeT* newNode;
-
-	newNode = (nodeT*)malloc(sizeof(nodeT));
-	printf("Please enter the data for the new node\n");
-	scanf("%d", &newNode->data);
-
-	temp = top;
-
-	for (int i = 0; i < pos - 2; i++)
+	//Tractors
+	if (machineType == 1 && machineCount > 0)
 	{
-		temp = temp->NEXT;
+		printf("Generating statistics for Tractor Machinery...");
+		printf("There are %f of the %d Tractors in the fleet have had 0 breakdowns.", &successesPercentage, machineCount);
 	}
 
-	temp2 = temp->NEXT;
-
-	temp->NEXT = newNode;
-	newNode->PREV = temp;
-	newNode->NEXT = temp2;
-	temp2->PREV = newNode;
-
-}
-
-void display(nodeT* top)
-{
-	nodeT* temp;
-
-	temp = top;
-
-	while (temp != NULL)
+	//Excavators
+	else if (machineType == 2 && machineCount > 0)
 	{
-		printf("The value of the node is %d\n", temp->data);
-
-		temp = temp->NEXT;
+		printf("Generating statistics for Excavator Machinery...");
+		printf("There are %f of the %d Excavators in the fleet have had 0 breakdowns.", &successesPercentage, machineCount);
 	}
 
-}
-
-void displayReverse(nodeT* end)
-{
-	nodeT* temp;
-
-	temp = end;
-
-	while (temp != NULL)
+	//Rollers
+	else if (machineType == 3 && machineCount > 0)
 	{
-		printf("The value of the node is %d\n", temp->data);
+		printf("Generating statistics for Roller Machinery...");
+		printf("There are %f of the %d Rollers in the fleet have had 0 breakdowns.", &successesPercentage, machineCount);
+	}
 
-		temp = temp->PREV;
+	//Cranes
+	else if (machineType == 4 && machineCount > 0)
+	{
+		printf("Generating statistics for Crane Machinery...");
+		printf("There are %f of the %d Cranes in the fleet have had 0 breakdowns.", &successesPercentage, machineCount);
+	}
+
+	//Mixers
+	else if (machineType == 5 && machineCount > 0)
+	{
+		printf("Generating statistics for Mixer Machinery...");
+		printf("There are %f of the %d Mixers in the fleet have had 0 breakdowns.", &successesPercentage, machineCount);
+	}
+
+	//No machines
+	else if (machineCount <= 0)
+	{
+		printf("There are no machines of the selected type in the fleet.");
 	}
 }
 
-int length(nodeT* top)
-{
-	nodeT* temp;
-	int count = 0;
+//listAll -> list all machines in order of current valuation
 
-	temp = top;
+//(ADMIN) displayMachine -> displays details of specific machine using inputted Chassis Number
 
-	while (temp != NULL)
-	{
-		count++;
-		temp = temp->NEXT;
-	}
+//(ADMIN) updateMachine -> update details of specific machine using inputted Chassis Number
 
-	return count;
+//deleteMachine -> delete machine based on chassis num
 
-}
-int search(nodeT* top, int searchTerm)
-{
-	nodeT* temp;
-	int count = 0;
-	int found = 0;
+//printAll -> calls on displayAll and then saveAll
 
-	temp = top;
+//loadAll -> loads machine fleet from file
 
-
-	while (temp != NULL)
-	{
-		count++;
-
-		if (temp->data == searchTerm)
-		{
-			found = 1;
-			break;
-		}
-
-		temp = temp->NEXT;
-
-	}
-
-	return found;
-
-}
-
-
-void deleteAtTheStart(nodeT** top, nodeT** end)
-{
-	nodeT* temp;
-
-
-	if (*top == NULL)
-	{
-		printf("Sorry the list is empty you can not delete from the list\n");
-	}
-	else if (*top == *end)
-	{
-		temp = *top;
-		*top = NULL;
-		*end = NULL;
-		free(temp);
-	}
-	else
-	{
-		temp = *top;
-		*top = temp->NEXT;
-		(*top)->PREV = NULL;
-		free(temp);
-
-
-	}
-
-
-}
-void deleteAtTheEnd(nodeT** top, nodeT** end)
-{
-	nodeT* temp;
-
-
-	if (*top == NULL)
-	{
-		printf("Sorry the list is empty you can not delete from the list\n");
-	}
-	else if (*top == *end)
-	{
-		temp = *top;
-		*top = NULL;
-		*end = NULL;
-		free(temp);
-	}
-	else
-	{
-		temp = *end;
-		*end = temp->PREV;
-		(*end)->NEXT = NULL;
-		free(temp);
-
-
-	}
-
-}
-*/
+//saveAll -> prints all machines + stats to file
